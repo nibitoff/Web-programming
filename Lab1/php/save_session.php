31997399ab2027b21a352acd3e15a5cf1c84cec2
nibibee <72893410+nibibee@users.noreply.github.com>
2021-09-28 21:24:10 +0300

    Create save_session.php

---
+++ Lab1/php/save_session.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+if (isset($_SESSION['results'])) {
+    foreach ($_SESSION['results'] as $result) { ?>
+        <tr>
+            <td><?php echo $result[0] ?></td>
+            <td><?php echo $result[1] ?></td>
+            <td><?php echo $result[2] ?></td>
+            <td class="<?php echo $result[3] ?>"><?php echo $result[3] ?></td>
+            <td><?php echo $result[5] ?></td>
+            <td><?php echo $result[4] ?></td>
+            <td><?php echo $result[6] ?></td>
+        </tr>
+    <?php }} ?>

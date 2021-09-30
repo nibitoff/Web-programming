1907bbb2c766b703dc7e80454ed68fb73a3ddfa0
nibibee <72893410+nibibee@users.noreply.github.com>
2021-09-30 15:05:16 +0300

    Update save_session.php

--- Lab1/php/save_session.php
+++ Lab1/php/save_session.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
-if (isset($_SESSION['results'])) {
-    foreach ($_SESSION['results'] as $result) { ?>
+if (isset($_SESSION['answers'])) {
+    foreach ($_SESSION['answers'] as $result) { ?>
         <tr>
             <td><?php echo $result[0] ?></td>
             <td><?php echo $result[1] ?></td>
             <td><?php echo $result[2] ?></td>
-            <td class="<?php echo $result[3] ?>"><?php echo $result[3] ?></td>
+            <td><?php echo $result[3] ?></td>
             <td><?php echo $result[5] ?></td>
             <td><?php echo $result[4] ?></td>
         </tr>

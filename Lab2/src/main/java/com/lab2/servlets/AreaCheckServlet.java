5a9a4cbd5a71a1dd9a60ea31227cec7557cb3905
Danil Sabitov <72893410+nibibee@users.noreply.github.com>
2021-11-11 16:51:43 +0300

    Update AreaCheckServlet.java

--- Lab2/src/main/java/com/lab2/servlets/AreaCheckServlet.java
+++ Lab2/src/main/java/com/lab2/servlets/AreaCheckServlet.java
@@ -46,7 +46,7 @@ public class AreaCheckServlet extends HttpServlet {
                         "<td>" + timeResponse + " ns" + "</td>" +
                         "<td>" + currentTime + "</td></tr>");
             } else {
-                message = "<td>Значение некорректно!</td>";
+                message = "<td>Г‡Г­Г Г·ГҐГ­ГЁГҐ Г­ГҐГЄГ®Г°Г°ГҐГЄГІГ­Г®!</td>";
                 answer.addFirst("<tr>" + message + message + message + message + message + message + "</tr>");
             }
 
@@ -61,9 +61,9 @@ public class AreaCheckServlet extends HttpServlet {
             if ((x <= 0 && y >= 0 && x * x + y * y <= r * r) ||
                     (x >= 0 && y <= 0 && x <= r / 2 && y <= r) ||
                     (x >= 0 && y >= 0 && y <= (-2 * x + r))) {
-                message = "Да";
+                message = "Г„Г ";
             } else {
-                message = "Нет";
+                message = "ГЌГҐГІ";
             }
         }
 
@@ -75,7 +75,6 @@ public class AreaCheckServlet extends HttpServlet {
             }
             return true;
         }
-
 
     }
 

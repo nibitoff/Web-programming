bcb572a1b833aa31013865ac7fa51427081cfbd0
nibibee <72893410+nibibee@users.noreply.github.com>
2021-11-11 03:47:16 +0300

    Create graphValidator.js

---
+++ Lab2/src/main/webapp/js/graphValidator.js
@@ -0,0 +1,61 @@
+function checkXArea(){
+    return x <= 4 && x >= -4;
+}
+
+function checkYArea(){
+    return y > -3 && y < 5;
+}
+
+function checkAreaCoordinates(){
+    let errorMsg;
+    if (checkXArea() && checkYArea()){
+        return true;
+    }
+    if (!checkXArea()){
+        errorMsg += "X должен находиться в диапазоне [-4;4]!"
+    }
+    if (!checkYArea()){
+        errorMsg += "\n Y должен находиться в диапазоне (-3;5)!"
+    }
+    alert(errorMsg);
+}
+
+function click(){
+    if (testR()){
+        convertCoordinates();
+        if (checkAreaCoordinates()){
+            sendRequest("no", x, y, r);
+            return false;
+        }
+        else {
+            unsetPoints();
+            return false;
+        }
+    }
+}
+
+function changeCoordinates(){
+    let centerX = 150;
+    let centerY = 150;
+
+    if (coordinatesX > centerX){
+        coordinatesX = coordinatesX - centerX;
+    }
+    else {
+        coordinatesX = -(centerX - coordinatesX);
+    }
+
+    if (coordinatesY > centerY){
+        coordinatesY = -(coordinatesY - centerY);
+    }
+    else {
+        coordinatesY = centerY - coordinatesY;
+    }
+}
+
+function convertCoordinates(){
+    changeCoordinates();
+    x = (coordinatesX / 120)*r;
+    y = (coordinatesY / 120)*r;
+}
+

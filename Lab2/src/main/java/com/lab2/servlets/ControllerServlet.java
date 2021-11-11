37823deec380ef974129797b310cbd2395047061
nibibee <72893410+nibibee@users.noreply.github.com>
2021-11-11 18:56:24 +0300

    Update ControllerServlet.java

--- Lab2/src/main/java/com/lab2/servlets/ControllerServlet.java
+++ Lab2/src/main/java/com/lab2/servlets/ControllerServlet.java
@@ -13,23 +13,36 @@ import java.util.LinkedList;
 public class ControllerServlet extends HttpServlet {
     HttpServletRequest request;
     HttpServletResponse response;
+    String check;
+    String x;
+    String y;
+    String r;
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String check = request.getParameter("check");
-        String x = request.getParameter("x");
-        String y = request.getParameter("y");
-        String r = request.getParameter("r");
+        check = request.getParameter("check");
+        x = request.getParameter("x");
+        y = request.getParameter("y");
+        r = request.getParameter("r");
 
-        if(check.equals("reload")){
-            response.sendRedirect("update.jsp");
-        }
-        else {
-            if ((x != null && y != null && r != null) && (!x.trim().equals("") && !y.trim().equals("") && !r.trim().equals(""))) {
+        if (checkNull()) {
+            if (check.equals("reload")) {
+                response.sendRedirect("update.jsp");
+            }else {
                 request.getRequestDispatcher("/AreaCheckServlet").forward(request, response);
-            } else {
-                response.setStatus(422);
             }
+            }else {
+            response.setStatus(422);
         }
     }
+
+    private boolean checkNull() {
+        if (check != null && !check.trim().equals("") && (x != null && y != null && r != null) && (!x.trim().equals("")
+                && !y.trim().equals("") && !r.trim().equals(""))) {
+            return true;
+        } else {
+            return false;
+        }
     }
+}
+

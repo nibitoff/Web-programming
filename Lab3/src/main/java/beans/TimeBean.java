e59f837188f3edfef47ea81563f902668c22f1a9
nibibee <72893410+nibibee@users.noreply.github.com>
2021-12-23 17:05:31 +0300

    Create TimeBean.java

---
+++ Lab3/src/main/java/beans/TimeBean.java
@@ -0,0 +1,17 @@
+package beans;
+
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+public class TimeBean implements Serializable {
+
+    public String learnTime(long time){
+        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");
+        String timeResp = formatter.format(new Date(System.currentTimeMillis()));
+        return timeResp;
+    }
+
+}
